337c16869ac3f6c903ee95d84c0489dee9d848e0
Vo Thanh Tuan <tuanvothanh.info@gmail.com>
2020-10-17 17:40:30 +0700

    Hide Intro

--- src/views/HomePage/index.jsx
+++ src/views/HomePage/index.jsx
@@ -27,7 +27,7 @@ const HomePage = () => {
       <Header />
       <Container className={classes.container}>
         <Carousel />
-        <Intro />
+        {/* <Intro /> */}
         <CourseList />
         <SuggestedCourses />
       </Container>
